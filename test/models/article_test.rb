16bb042facd0813695b39c7e5abe8bf9d1bb41a4
felix cohen <felix@Ishtar.local>
2020-03-11 16:32:14 +0000

    add second test

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,10 +1,18 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  test "Author attributes after or on scopes is broken" do
+  test "Author attribute after or on scopes is broken without reload" do
     author = Author.find_by_name('one')
     articles = author.articles.or(author.co_authored_articles)
     author_names = articles.map { |article| article.author.name }
     assert_equal(["one", "one"], author_names)
+  end
+
+  test "Author attributes after or on scopes is correct with reload" do
+    author = Author.find_by_name('one')
+    articles = author.articles.or(author.co_authored_articles)
+    articles.each { |article| article.reload }
+    author_names = articles.map { |article| article.author.name }
+    assert_equal(["two", "one"], author_names)
   end
 end
